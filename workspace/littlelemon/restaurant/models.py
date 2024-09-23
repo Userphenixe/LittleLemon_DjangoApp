05f5501534dfdc09029da887c51a1f1e395568c7
Userphenixe <hamabbas910@gmail.com>
2024-09-24 00:45:27 +0100

    Config Models

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateField(auto_now_add= True)
 
-# Create your models here.
+    def __str__(self):
+        return str(self.Name)
+    
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.ImageField()
+
+    def __str__(self):
+        return str(self.Title)
