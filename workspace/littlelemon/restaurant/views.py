77f48eb0b8da9fe54145d7d5037df0c9f7a5fcb8
Userphenixe <hamabbas910@gmail.com>
2024-09-25 00:24:07 +0100

    Authentification Token

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -3,16 +3,20 @@ from .serializers import SerializerMenu, SerializerBooking
 from rest_framework import generics
 from rest_framework import viewsets
 from .models import Menu, Booking
+from rest_framework.permissions import IsAuthenticated
 
 def index(request):
     return render(request, 'index.html', {})
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = SerializerMenu
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     ueryset = Menu.objects.all()
     serializer_class = SerializerMenu
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = SerializerBooking
